f222746b79edea3349de5ec8b186d2d4f92d7e10
Pablo María Arranz Pou <71595730+pablomariaarranzpou@users.noreply.github.com>
2023-01-29 20:42:40 +0100

    Time: 128 ms (31.42%), Space: 14.3 MB (95.18%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.py
@@ -0,0 +1,47 @@
+class Solution:
+
+    def __init__(self):
+        self.sols = []
+
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        
+        board = ["."*n for _ in range(n)] 
+        self.solve_queens_backtracking(n, board, 0)
+        return self.sols
+    
+    def solve_queens_backtracking(self, N, board, col):
+        
+        if(col == N):
+            print(board)
+            self.sols.append(board.copy())
+       
+        for i in range(N):
+            
+            if (self.check_position_previous_columns(board, i, col)):
+                board[i] = board[i][:col] + 'Q' + board[i][col + 1:]
+                
+                if self.solve_queens_backtracking(N, board, col + 1):
+                    return board
+            
+                board[i] = board[i][:col] + '.' + board[i][col + 1:]
+            
+        return False
+
+    def check_position_previous_columns(self, board, row, col):
+    
+        # Comprovem les columnes anteriors de la fila 'row'
+        for i in range(col):
+            if board[row][i] == 'Q':
+                return False
+
+        # Comprovem la diagonal inferior
+        for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+            if board[i][j] == 'Q':
+                return False
+
+        # Comprovem la diagonal superior
+        for i, j in zip(range(row, len(board), 1), range(col, -1, -1)):
+            if board[i][j] == 'Q':
+                return False
+
+        return True
