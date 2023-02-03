1497f5ad2060991140a042ff33b333dad3d3bb6f
Pablo María Arranz Pou <71595730+pablomariaarranzpou@users.noreply.github.com>
2023-02-03 21:03:08 +0100

    Time: 41 ms (51.13%), Space: 13.9 MB (66.62%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -6,7 +6,7 @@ class Solution:
         n = len(matrix)
         
         for i in range(n):
-            for j in range(i, n):
+            for j in range(i + 1, n):
                 
                 aux = matrix[i][j]
                 matrix[i][j] = matrix[j][i]
