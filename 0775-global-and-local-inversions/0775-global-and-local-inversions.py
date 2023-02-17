dba89d7a8c68adba5dd2ba00979926e4b87aa35d
Pablo María Arranz Pou <71595730+pablomariaarranzpou@users.noreply.github.com>
2023-02-17 11:44:44 +0100

    Time: 843 ms (73.33%), Space: 27.8 MB (17.78%) - LeetHub

---
+++ 0775-global-and-local-inversions/0775-global-and-local-inversions.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isIdealPermutation(self, nums: List[int]) -> bool:
+        for i in range(len(nums)):
+            if abs(nums[i]-i)>1:
+                return False
+        return True
+                
+        
+                
+
+        
