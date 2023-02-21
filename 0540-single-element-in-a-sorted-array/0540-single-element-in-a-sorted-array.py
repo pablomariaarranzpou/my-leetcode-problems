ef0a0a769df3f1676ef5e26ec498f291ec2214f7
Pablo María Arranz Pou <71595730+pablomariaarranzpou@users.noreply.github.com>
2023-02-21 22:59:48 +0100

    Time: 183 ms (51.22%), Space: 23.7 MB (38.99%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -1,10 +1,6 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        
-        visited = set()
+        res = 0
         for num in nums:
-            if num not in visited:
-                visited.add(num)
-            else:
-                visited.remove(num)
-        return visited.pop()
+            res ^= num
+        return res
