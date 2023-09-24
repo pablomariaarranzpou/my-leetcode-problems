8c4b213d3237dc5466242dba93e268e95578d926
Pablo María Arranz Pou <71595730+pablomariaarranzpou@users.noreply.github.com>
2023-09-24 13:31:49 +0200

    Time: 50 ms (5.33%), Space: 16.1 MB (82.07%) - LeetHub

---
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.py
@@ -0,0 +1,9 @@
+class Solution:
+    def checkString(self, s: str) -> bool:
+        
+        for i in range(0, len(s) - 1):
+            
+            if int(s[i] > s[i + 1]):
+                return False
+        return True
+        
