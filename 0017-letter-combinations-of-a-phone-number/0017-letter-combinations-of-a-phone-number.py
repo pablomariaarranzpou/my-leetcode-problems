656707f8c9cff6f19aca77cee0086c9fbca72b5c
Pablo María Arranz Pou <71595730+pablomariaarranzpou@users.noreply.github.com>
2024-04-28 12:27:28 +0200

    Time: 38 ms (36.77%), Space: 16.5 MB (41.30%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -2,14 +2,14 @@ class Solution:
     
     import itertools
     def letterCombinations(self, digits: str) -> List[str]:
+        
+        if not digits:
+            return []
 
         numbers = {'2':'abc', '3':'def', '4':'ghi', '5':'jkl', '6':'mno',
                    '7':'pqrs', '8':'tuv', '9':'wxyz'}
-
-        product = []
-        if not digits:
-            return []
-        response = []
+        
+        product, response = [], []
         
         for digit in digits:
             product.append(numbers[digit])
