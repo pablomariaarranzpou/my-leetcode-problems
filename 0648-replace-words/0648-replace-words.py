fcef7eb3e11ddf8f55a673c528aba18030998753
Pablo María Arranz Pou <71595730+pablomariaarranzpou@users.noreply.github.com>
2024-06-07 16:53:53 +0200

    Time: 188 ms (36.76%), Space: 24.9 MB (81.26%) - LeetHub

--- 0648-replace-words/0648-replace-words.py
+++ 0648-replace-words/0648-replace-words.py
@@ -2,10 +2,11 @@ class Solution:
     def replaceWords(self, dictionary: List[str], sentence: str) -> str:
         sentence = sentence.split(" ")
         result = ""
+        dictionary = sorted(dictionary, key=len)[::-1]
         for word in sentence:
             string = word 
             for lex in dictionary:
-                if lex == word[:len(lex)] and len(lex) < len(string):
+                if lex == word[:len(lex)]:
                     string = lex 
             result += string + " "  
         return result[:-1]
