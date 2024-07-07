923093e3532d7994bf31800695445fd84e7eb4ce
Pablo María Arranz Pou <71595730+pablomariaarranzpou@users.noreply.github.com>
2024-07-07 20:01:06 +0200

    Time: 53 ms (5.97%), Space: 16.5 MB (79.17%) - LeetHub

---
+++ 1518-water-bottles/1518-water-bottles.py
@@ -0,0 +1,12 @@
+import math
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        
+        res = 0
+        
+        while(numBottles >= numExchange):
+            res += numExchange
+            numBottles -= numExchange
+            numBottles += 1
+            
+        return res + numBottles
