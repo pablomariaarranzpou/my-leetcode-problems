da6d72c85b9cb7806084e95984feb906e16ddab2
Pablo María Arranz Pou <71595730+pablomariaarranzpou@users.noreply.github.com>
2024-07-09 20:43:13 +0200

    Time: 774 ms (21.31%), Space: 57.4 MB (9.97%) - LeetHub

--- 1701-average-waiting-time/1701-average-waiting-time.py
+++ 1701-average-waiting-time/1701-average-waiting-time.py
@@ -3,17 +3,14 @@ class Solution:
         
         total = customers[0][1] + customers[0][0]
         calc = customers[0][1]
-        
+
         for i in range(1, len(customers)):
+            # Si el chef llega con tiempo
             if(total < customers[i][0]):
                 calc += customers[i][1]
-                total = (customers[i][1] + customers[i][0])
-                
+                total = (customers[i][1] + customers[i][0])    
             else:
+                # Si el chef llega con retraso
                 calc += (total - customers[i][0] + customers[i][1])
                 total += customers[i][1]
-                
-                
- 
-        return calc / len(customers)
-            
+        return calc / len(customers)    
