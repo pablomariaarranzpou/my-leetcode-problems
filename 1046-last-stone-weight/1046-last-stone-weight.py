139bbced17855fbf7154b9c12b9f8cd5c1c2e225
Pablo María Arranz Pou <71595730+pablomariaarranzpou@users.noreply.github.com>
2024-07-12 14:33:09 +0200

    Time: 37 ms (48.54%), Space: 16.3 MB (99.52%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -1,20 +1,13 @@
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
-
-  
         while len(stones) > 1:
-        
             stones = sorted(stones)
-            
-
             stones[-2] = stones[-1] - stones[-2]
-            
             stones.pop()
-            
-  
-        if len(stones) == 0:
-            return 0
-        else:
+
+        if stones:
             return stones[0]
+        else:
+            return 0
                 
                 
