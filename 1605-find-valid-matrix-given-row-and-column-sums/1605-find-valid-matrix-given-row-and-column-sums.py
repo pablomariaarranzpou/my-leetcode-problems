814d57b0815c1b76d8c73aec998638de05fe5142
Pablo María Arranz Pou <71595730+pablomariaarranzpou@users.noreply.github.com>
2024-07-20 14:53:34 +0200

    Time: 462 ms (77.62%), Space: 22.1 MB (14.08%) - LeetHub

--- 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.py
+++ 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.py
@@ -6,7 +6,15 @@ class Solution:
             for j in range(len(colSum)):
                 r_v = rowSum[i]
                 c_v = colSum[j]
-                if(r_v == 0 or r_v <= c_v):
+                if(r_v == 0):
+                    r.append(r_v)
+                    rowSum[i] -= r_v
+                    colSum[j] -= r_v
+                elif(c_v == 0):
+                    r.append(c_v)
+                    rowSum[i] -= c_v
+                    colSum[j] -= c_v
+                elif(r_v <= c_v):
                     r.append(r_v)
                     rowSum[i] -= r_v
                     colSum[j] -= r_v
