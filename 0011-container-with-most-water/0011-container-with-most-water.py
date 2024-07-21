dc44c9c6bbe4e3b50f53e5f8e944a5a49cd49917
Pablo María Arranz Pou <71595730+pablomariaarranzpou@users.noreply.github.com>
2024-07-21 19:26:27 +0200

    Time: 493 ms (89.01%), Space: 30 MB (10.27%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,41 +1,16 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        
-        
-        # diferencia de max(i) - min(i) * min(nums)
-        
         front = 0
-        
         rear = len(height) - 1
-        
-        
         maxi = 0
-        
-        
         while front < rear:
-            
             h_f = height[front]
             h_r = height[rear]
             area = (rear - front) * min(h_f,h_r)
-            
             if area > maxi: 
-                maxi = area
-                
+                maxi = area  
             if h_f > h_r:
                 rear -= 1
             else:
-                front += 1
-                
-        return maxi
-                
-                
-        
-
-       
-            
-           
-            
-            
-            
-            
-        
+                front += 1       
+        return maxi             
