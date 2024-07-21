5aca81e5fdf10ed48fafb8c4c49bb4b754559bef
Pablo María Arranz Pou <71595730+pablomariaarranzpou@users.noreply.github.com>
2024-07-21 17:48:04 +0200

    Time: 57 ms (73.55%), Space: 17.7 MB (24.80%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,22 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        nums_with_indices = [(num, i) for i, num in enumerate(nums)]
         
-        nums_with_indices.sort()
-
-        front = 0
-        rear = len(nums) - 1
-
-        while front < rear:
-            current_sum = nums_with_indices[front][0] + nums_with_indices[rear][0]
-            
-            if current_sum == target:
-                return [nums_with_indices[front][1], nums_with_indices[rear][1]]
-            elif current_sum < target:
-                front += 1
-            else:
-                rear -= 1
-
-        return []
+        nums_s = set(nums)
+        
+        for i in range(len(nums)):
+            opt = target - nums[i]
+            if(opt in nums_s):
+                index = nums.index(opt)
+                if  index != i:
+                    return [nums.index(opt), i]
             
   
