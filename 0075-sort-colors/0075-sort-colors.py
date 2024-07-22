5a6640d01796b2ce802511379f3c8b1ef224da25
Pablo María Arranz Pou <71595730+pablomariaarranzpou@users.noreply.github.com>
2024-07-22 22:31:45 +0200

    Time: 41 ms (30.15%), Space: 16.4 MB (92.64%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -4,4 +4,22 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         
-        nums.sort()
+        r, w = 0, 0
+            
+        for i in nums:
+            
+            if i == 0:
+                r += 1
+            elif i == 1:
+                w += 1
+
+                
+        nums[:r] = [0] * r
+        
+        nums[r:w+r] = [1] * w
+        
+        nums[w+r:] = [2] * (len(nums) - w - r)
+        
+            
+            
+        
