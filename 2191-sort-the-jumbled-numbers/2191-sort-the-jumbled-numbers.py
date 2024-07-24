eb0e6df682fbf36005a789429091215b3be33b77
Pablo María Arranz Pou <71595730+pablomariaarranzpou@users.noreply.github.com>
2024-07-24 10:51:11 +0200

    Time: 1231 ms (18.48%), Space: 26.8 MB (16.85%) - LeetHub

--- 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.py
+++ 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.py
@@ -3,7 +3,12 @@ class Solution:
         def map_number(num: int) -> int:
             mapped_digits = [str(mapping[int(digit)]) for digit in str(num)]
             return int("".join(mapped_digits))
+
         mapped_nums = [(map_number(num), index, num) for index, num in enumerate(nums)]
+
         mapped_nums.sort(key=lambda x: (x[0], x[1]))
+
         return [num[2] for num in mapped_nums]
+
+        return res
         
