eb3e612474c416749b7b68be0795dee094bbb74c
Pablo María Arranz Pou <71595730+pablomariaarranzpou@users.noreply.github.com>
2024-07-28 12:53:22 +0200

    Time: 2347 ms (15.89%), Space: 26.8 MB (21.85%) - LeetHub

---
+++ 2045-second-minimum-time-to-reach-destination/2045-second-minimum-time-to-reach-destination.py
@@ -0,0 +1,48 @@
+import heapq
+class Solution:
+    def secondMinimum(self, n: int, edges: List[List[int]], time: int, change: int) -> int:
+        
+        pq = [(0, 1)]
+        visited = set()
+        graph = {}
+        distance = {i: [float('inf'), float('inf')] for i in range(1, n + 1)}
+        distance[1][0] = 0
+            
+            
+        for edge in edges:
+            
+            if edge[0] not in graph.keys():
+                graph[edge[0]] = [edge[1]]
+            else:
+                graph[edge[0]].append(edge[1])
+                
+                
+            if edge[1] not in graph.keys():
+                graph[edge[1]] = [edge[0]]
+            else:
+                graph[edge[1]].append(edge[0]) 
+                
+        
+        while pq:
+            
+            act_time, node = heapq.heappop(pq)
+                
+            for neihgbour in graph[node]:
+                
+                
+                new_time = act_time + time
+
+                if (act_time // change) % 2 == 1:
+                    new_time = (act_time // change + 1) * change + time
+                
+                if new_time < distance[neihgbour][0]:
+                    distance[neihgbour][1] = distance[neihgbour][0]
+                    distance[neihgbour][0] = new_time
+                    heapq.heappush(pq, (new_time, neihgbour))
+                elif distance[neihgbour][0] < new_time < distance[neihgbour][1]:
+                    distance[neihgbour][1] = new_time
+                    heapq.heappush(pq, (new_time, neihgbour))
+                    
+
+        return distance[n][1]
+        
