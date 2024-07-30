54a51a206eb4681594503b0823f5a29e816b0e16
Pablo María Arranz Pou <71595730+pablomariaarranzpou@users.noreply.github.com>
2024-07-30 20:53:06 +0200

    Time: 38 ms (30.33%), Space: 16.4 MB (53.65%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -13,9 +13,8 @@ class Solution:
         dp[1] = 1
         dp[2] = 1
 
-        for i in range(2, n + 1):
-
+        for i in range(3, n+1):
+            
             dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
 
-        return dp[n]
-
+        return dp[-1]
