e1cf3e66766e7474c3ac6eb9dcbe842948cc5b44
Pablo María Arranz Pou <71595730+pablomariaarranzpou@users.noreply.github.com>
2024-08-06 14:44:45 +0200

    Time: 136 ms (94.51%), Space: 17.7 MB (36.75%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
@@ -4,21 +4,19 @@ class Solution:
         if not word:
             return 0
         
-        values = [0] + sorted(Counter(word).values(), reverse=True)
+        values = sorted(Counter(word).values(), reverse=True)
         
         factor = 1
         total = 0
         
         
-        for i in range(1, len(values)):
+        for i in range(len(values)):
             
             total += (factor * values[i])
             
-            if(i % 8 == 0):
+            if((i+1) % 8 == 0):
                 factor += 1
-
-            
-            
+                
         return total
             
         
