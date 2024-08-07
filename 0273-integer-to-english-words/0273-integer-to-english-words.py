4167ce7c916a901f55e075c385b7c70a3483f788
Pablo María Arranz Pou <71595730+pablomariaarranzpou@users.noreply.github.com>
2024-08-07 18:37:19 +0200

    Time: 38 ms (47.54%), Space: 16.8 MB (7.73%) - LeetHub

---
+++ 0273-integer-to-english-words/0273-integer-to-english-words.py
@@ -0,0 +1,82 @@
+class Solution:
+    
+    def __init__(self):
+        self.word = ""
+        
+    def numberToWords(self, num: int) -> str:
+        
+        if num == 0:
+            return "Zero"
+        
+        if num == 1:
+            return "One"
+        
+        nums = {
+            1: "One", 2: "Two", 3: "Three", 4: "Four",
+            5: "Five", 6: "Six", 7: "Seven", 8: "Eight",
+            9: "Nine", 10: "Ten", 11: "Eleven", 12: "Twelve",
+            13: "Thirteen", 14: "Fourteen", 15: "Fifteen",
+            16: "Sixteen", 17: "Seventeen", 18: "Eighteen",
+            19: "Nineteen"
+        }
+        
+        dic = {20: "Twenty", 30: "Thirty", 40: "Forty",
+            50: "Fifty", 60: "Sixty", 70: "Seventy",
+            80: "Eighty", 90: "Ninety",
+            100: "Hundred",
+            1000: "Thousand",
+            1000000: "Million",
+            1000000000: "Billion"
+        }
+        
+        
+        def auxiliar_function(num, word):
+            
+            if num == 0:
+                pass  
+            elif num in nums.keys():
+                self.word += " " + nums[num]
+            else:
+            
+                for div in sorted(dic.keys(), reverse=True):
+                    
+                    if num / div < 1:
+                        continue
+                    
+                    if div >= 100:
+                        
+                        calc = num // div
+                        mod = num % div
+                        
+                        auxiliar_function(calc, self.word)
+                        self.word += " " + dic[div]
+                        if mod != 0:
+                            auxiliar_function(mod, self.word)
+                    else:
+                        calc = div
+                        mod = num - div
+                        
+                        self.word += " " + dic[div]
+                        
+                        if mod != 0:
+                            auxiliar_function(mod, self.word)
+                    break
+                                  
+        auxiliar_function(num, self.word)
+        
+        return self.word[1:]
+        
+        
+            
+        
+            
+                    
+            
+            
+                    
+            
+                    
+                    
+            
+        
+        
