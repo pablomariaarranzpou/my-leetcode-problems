28b2535c2d48ccd93ce50aec94240734ef3d23d6
Pablo María Arranz Pou <71595730+pablomariaarranzpou@users.noreply.github.com>
2024-08-08 12:27:26 +0200

    Time: 80 ms (71.09%), Space: 17.7 MB (74.61%) - LeetHub

--- 0885-spiral-matrix-iii/0885-spiral-matrix-iii.py
+++ 0885-spiral-matrix-iii/0885-spiral-matrix-iii.py
@@ -14,44 +14,41 @@ class Solution:
         
         while radio <= max_radio:
             
-        
-            path.append([i, j])
-            
-            print(i + 1, rStart + radio)        
+            if j < cols and i < rows and i >= 0 and j >= 0:
+                path.append([i, j])
+                    
             while i + 1 < rStart + radio + 1:
                 # abajo sumamos una row 
-                path.append([i + 1, j])
+                if j < cols and i + 1 < rows and i + 1 >= 0 and j >= 0:
+                    path.append([i + 1, j])
                 i += 1
-            
-            print(j - 1, cStart - radio) 
+             
             while j - 1 > cStart - radio - 1:
                 
                 # izquierda restamos una columna
-                path.append([i, j - 1])
+                if j - 1 < cols and i < rows and i >= 0 and j - 1>= 0:
+                    path.append([i, j - 1])
                 j -= 1
                     
             while i - 1 > rStart - radio - 1:
                 # arriba restamos una row
-                path.append([i - 1, j])
+                
+                if j < cols and i - 1 < rows and i - 1 >= 0 and j >= 0:
+                    path.append([i - 1, j])
                 i -= 1
                      
             while j + 1 < cStart + radio + 1:
                 
                 # derecha sumamos una col
-                path.append([i, j + 1])
+                
+                if j + 1 < cols and i < rows and i >= 0 and j + 1 >= 0:
+                    path.append([i, j + 1])
                 j += 1
                     
             j += 1
             radio += 1
             
-        final = []
-        for edge in path:
-            
-            if edge[0] >= 0 and edge[1] >= 0 and edge[0] < rows and edge[1] < cols and edge not in final:
-                final.append(edge)
-            
-        return final
-                
+        return path
                 
             
                 
