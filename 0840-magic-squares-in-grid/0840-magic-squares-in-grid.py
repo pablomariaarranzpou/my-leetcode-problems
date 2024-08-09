6449e2b0e260f78137fed7eafeb330294327bc01
Pablo María Arranz Pou <71595730+pablomariaarranzpou@users.noreply.github.com>
2024-08-09 12:24:47 +0200

    Time: 46 ms (21.03%), Space: 16.3 MB (98.46%) - LeetHub

---
+++ 0840-magic-squares-in-grid/0840-magic-squares-in-grid.py
@@ -0,0 +1,64 @@
+class Solution:
+    def numMagicSquaresInside(self, grid: List[List[int]]) -> int:
+        
+        
+        def isMagic(matrix):
+            
+            visited = set()
+            for i in range(3):
+                for j in range(3):
+                    
+                    num = matrix[i][j]
+    
+                    if  0 < num < 10 and num not in visited:
+                        visited.add(num)
+                        
+                    else:
+                        return False
+                        
+                    
+                    
+            
+            suma_r1 = sum(matrix[0])
+            suma_r2 = sum(matrix[1])
+            
+            if suma_r1 != suma_r2:
+                return False
+            
+            suma_r3 = sum(matrix[2])
+            
+            if suma_r3 != suma_r2:
+                return False
+            
+            suma_col1 = matrix[0][0] + matrix[1][0] + matrix[2][0]
+            suma_col2 = matrix[0][1] + matrix[1][1] + matrix[2][1]
+            suma_col3 = matrix[0][2] + matrix[1][2] + matrix[2][2]
+            
+            suma_d1 = matrix[0][0] + matrix[1][1] + matrix[2][2]
+            suma_d2 = matrix[0][2] + matrix[1][1] + matrix[2][0]
+        
+            
+            if suma_col1 == suma_col2 == suma_col3 == suma_d1 == suma_d2 == suma_r1 == suma_r2 == suma_r3:
+                return True
+            else:
+                return False
+            
+            return True
+               
+            
+        total = 0
+        rows = len(grid)
+        cols = len(grid[0])
+        
+        for i in range(rows - 2):
+            for j in range(cols - 2):
+                
+                matrix = [[grid[a][b] for b in range(j, j + 3)] for a in range(i, i + 3)]
+                
+                if isMagic(matrix):
+                    total += 1
+                    
+        
+        return total
+                
+                
