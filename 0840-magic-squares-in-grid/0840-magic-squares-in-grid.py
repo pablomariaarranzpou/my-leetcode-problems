3a9e218486d185178456ab2fbcd4cdff95bca612
Pablo María Arranz Pou <71595730+pablomariaarranzpou@users.noreply.github.com>
2024-08-09 12:26:46 +0200

    Time: 37 ms (81.03%), Space: 16.6 MB (53.33%) - LeetHub

--- 0840-magic-squares-in-grid/0840-magic-squares-in-grid.py
+++ 0840-magic-squares-in-grid/0840-magic-squares-in-grid.py
@@ -14,10 +14,7 @@ class Solution:
                         visited.add(num)
                         
                     else:
-                        return False
-                        
-                    
-                    
+                        return False        
             
             suma_r1 = sum(matrix[0])
             suma_r2 = sum(matrix[1])
@@ -31,17 +28,29 @@ class Solution:
                 return False
             
             suma_col1 = matrix[0][0] + matrix[1][0] + matrix[2][0]
+            
+            if suma_col1 != suma_r2:
+                return False
+            
             suma_col2 = matrix[0][1] + matrix[1][1] + matrix[2][1]
+            
+            if suma_col2 != suma_r2:
+                return False
+            
             suma_col3 = matrix[0][2] + matrix[1][2] + matrix[2][2]
             
-            suma_d1 = matrix[0][0] + matrix[1][1] + matrix[2][2]
-            suma_d2 = matrix[0][2] + matrix[1][1] + matrix[2][0]
-        
-            
-            if suma_col1 == suma_col2 == suma_col3 == suma_d1 == suma_d2 == suma_r1 == suma_r2 == suma_r3:
-                return True
-            else:
+            if suma_col3 != suma_r2:
                 return False
+            
+            suma_d1 = matrix[0][0] + matrix[1][1] + matrix[2][2]
+            
+            if suma_d1 != suma_r2:
+                return False
+            suma_d2 = matrix[0][2] + matrix[1][1] + matrix[2][0]
+            
+            if suma_d2 != suma_r2:
+                return False
+        
             
             return True
                
