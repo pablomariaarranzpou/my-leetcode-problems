251bd6b36302e7c646dabd8c2d1fee58bc58cb6d
Pablo María Arranz Pou <71595730+pablomariaarranzpou@users.noreply.github.com>
2024-08-16 21:22:23 +0200

    Time: 1300 ms (96.26%), Space: 41.9 MB (74.71%) - LeetHub

--- 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.py
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.py
@@ -11,24 +11,26 @@ class Solution:
         
         for index, subarray in enumerate(arrays):
             
-            if subarray[-1] > max_value:
+            cand_max = subarray[-1]
+            cand_min = subarray[0]
+            
+            if cand_max > max_value:
                 aux = max_value
-                max_value = subarray[-1]
+                max_value = cand_max
                 max_value_index = index
                 second_max_value = aux
                 
             elif subarray[-1] > second_max_value:
-                second_max_value = subarray[-1]
+                second_max_value = cand_max
                 
-            if subarray[0] < min_value:
+            if cand_min < min_value:
                 aux = min_value
-                min_value = subarray[0]
+                min_value = cand_min
                 min_value_index = index
                 second_min_value = aux
                 
-            elif subarray[0] < second_min_value:
-                second_min_value = subarray[0]
-                
+            elif cand_min < second_min_value:
+                second_min_value = cand_min
                 
         if max_value_index != min_value_index:
             return max_value - min_value
