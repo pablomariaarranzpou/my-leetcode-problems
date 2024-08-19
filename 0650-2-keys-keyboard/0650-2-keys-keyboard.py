b147d9af5a7667443b8fb8535e4677dc7cfd6569
Pablo María Arranz Pou <71595730+pablomariaarranzpou@users.noreply.github.com>
2024-08-19 12:17:56 +0200

    Time: 128 ms (54.33%), Space: 27.7 MB (20.61%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.py
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -1,7 +1,7 @@
 class Solution:
     def minSteps(self, n: int) -> int:
         
-        
+        memo = {}
         if n == 1:
             return 0
         
@@ -13,13 +13,19 @@ class Solution:
             if a_count > n:
                 return float('inf')
             
-            
+            if (a_count, copied) in memo:
+                return memo[(a_count, copied)]
             
             pasting = 1 + recuersive(a_count + copied, copied)
             copi_paste = 2 + recuersive(a_count * 2, a_count)
             
+            mini = min(pasting, copi_paste)
             
-            return min(pasting, copi_paste)
+            memo[(a_count, copied)] = mini
+            
+            return mini
+                
+            
         
         return 1 + recuersive(1,1)
         
