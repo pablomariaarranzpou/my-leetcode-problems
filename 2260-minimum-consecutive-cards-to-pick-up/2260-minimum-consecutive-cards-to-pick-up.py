a59d3ab431101fab415c346ba2d79ae9c598bd26
Pablo María Arranz Pou <71595730+pablomariaarranzpou@users.noreply.github.com>
2024-08-21 19:53:53 +0200

    Time: 670 ms (28.01%), Space: 43.5 MB (20.26%) - LeetHub

--- 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.py
+++ 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.py
@@ -1,16 +1,22 @@
 class Solution:
     def minimumCardPickup(self, cards: List[int]) -> int:
         
+        minimum = float('inf')
         hashmp = {}
         for index, card in enumerate(cards):
             
             if card in hashmp.keys():
+                
+                if index - hashmp[card][-1] < minimum:
+                    minimum = index - hashmp[card][-1]
+                
                 hashmp[card].append(index)
             else:
+                
                 hashmp[card] = [index]
         
-        minimum = float('inf')
-                
+        
+        """
         for key in hashmp.keys():
             
             appaerences = hashmp[key]
@@ -20,7 +26,7 @@ class Solution:
                     #print(appaerences[aparition + 1], "-", appaerences[aparition])
                     if appaerences[aparition + 1] - appaerences[aparition] < minimum:
                         minimum = appaerences[aparition + 1] - appaerences[aparition]
-                        
+        """
         if minimum == float('inf'):
             return -1
         else:
