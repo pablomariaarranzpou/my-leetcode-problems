68e4892430ac4c31574863f0fa2654c3ebe6632c
Pablo María Arranz Pou <71595730+pablomariaarranzpou@users.noreply.github.com>
2024-08-22 17:38:18 +0200

    Time: 79 ms (33.13%), Space: 16.7 MB (11.98%) - LeetHub

--- 2848-points-that-intersect-with-cars/2848-points-that-intersect-with-cars.py
+++ 2848-points-that-intersect-with-cars/2848-points-that-intersect-with-cars.py
@@ -3,26 +3,18 @@ class Solution:
         
         nums = sorted(nums, key=lambda x: x[0])
         suma = 0
-        
-        
         mini = nums[0][0]
         maxi = nums[0][1]
-        
         suma = maxi - mini + 1
-        
-        print(nums)
         for i in range(1, len(nums)):
-            
             if nums[i][0] <= maxi <= nums[i][1]:
                 mini = maxi + 1
             elif maxi > nums[i][1]:
                 continue
             else:
                 mini = nums[i][0]
-            print(suma)
             maxi = nums[i][1]
             suma += maxi - mini + 1
-        
         return suma
                 
                 
