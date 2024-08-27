3a3ecab0e4844aff386b78ea96ec45308e1e1d28
Pablo María Arranz Pou <71595730+pablomariaarranzpou@users.noreply.github.com>
2024-08-27 12:19:37 +0200

    Time: 1030 ms (6.38%), Space: 27.4 MB (100.00%) - LeetHub

---
+++ 2931-maximum-spending-after-buying-items/2931-maximum-spending-after-buying-items.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maxSpending(self, values: List[List[int]]) -> int:
+        
+        c = 0
+        n = len(values)
+        m = len(values[0])
+        for d in range(1, n * m + 1):
+            
+            min_value = float('inf')
+            min_shop = None
+            
+            for i in range(len(values)):
+                
+                if values[i]:
+                    if min_value > values[i][-1]:
+                        min_shop = i
+                        min_value = values[i][-1]
+                        
+                    
+            c += values[min_shop].pop() * d
+                
+        return c
+                        
+                        
+                
+                        
+                    
+                
+        
