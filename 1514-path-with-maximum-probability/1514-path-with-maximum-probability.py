1e04b79562557124b076bc434cc3b83f554ec262
Pablo María Arranz Pou <71595730+pablomariaarranzpou@users.noreply.github.com>
2024-08-31 15:30:52 +0200

    Time: 562 ms (45.36%), Space: 29.8 MB (19.10%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
@@ -2,36 +2,38 @@ class Solution:
     def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
         
         graph = defaultdict(list)
+        max_prob = {}
         
         for i in range(len(edges)):
             if succProb[i]:
                 prob = -(math.log(succProb[i]))
-                graph[edges[i][0]].append((edges[i][1], prob))
-                graph[edges[i][1]].append((edges[i][0], prob))
+                graph[edges[i][0]].append((prob, edges[i][1]))
+                graph[edges[i][1]].append((prob, edges[i][0]))
         
-        dist = {start_node: 0}
+        
+        for i in range(n):
+            max_prob[i] = float('inf')
+                
+        max_prob[start_node] = 0
         pq = [(0, start_node)]
         
         visited = set()
         
         
         while pq:
+            
             prob_node, node = heapq.heappop(pq)
             
             if node == end_node:
                 return math.exp(-prob_node)
             
-            if node not in visited:
-                
-                visited.add(node)
-            
-                for neig_node, neig_prob in graph[node]:
+            for neig_prob, neig in graph[node]:
                     
-                    calc = prob_node + neig_prob
+                calc = prob_node + neig_prob
 
-                    if calc < dist.get(neig_node, float('inf')):
-                        dist[neig_node] = calc
-                        heapq.heappush(pq, (calc, neig_node))
+                if calc < max_prob[neig]:
+                    max_prob[neig] = calc
+                    heapq.heappush(pq, (calc, neig))
     
         return 0.0
 
