d427955f26eece464cf2587fb450817fa64dad09
Pablo María Arranz Pou <71595730+pablomariaarranzpou@users.noreply.github.com>
2024-08-31 13:54:44 +0200

    Time: 546 ms (68.23%), Space: 30 MB (5.32%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
@@ -3,32 +3,28 @@ class Solution:
         
         graph = defaultdict(list)
         
-        # Construir el grafo usando -log(p)
         for i in range(len(edges)):
-            prob = -math.log(succProb[i])
-            graph[edges[i][0]].append((edges[i][1], prob))
-            graph[edges[i][1]].append((edges[i][0], prob))
+            if succProb[i]:
+                prob = -(math.log(succProb[i]))
+                graph[edges[i][0]].append((edges[i][1], prob))
+                graph[edges[i][1]].append((edges[i][0], prob))
         
-        # Inicialización de la cola de prioridad y distancias
         dist = {start_node: 0}
         pq = [(0, start_node)]
         
         while pq:
             prob_node, node = heapq.heappop(pq)
             
-            # Si llegamos al nodo objetivo, devolvemos el resultado
             if node == end_node:
                 return math.exp(-prob_node)
             
-            # Procesar los vecinos
             for neig_node, neig_prob in graph[node]:
                 calc = prob_node + neig_prob
                 
                 if calc < dist.get(neig_node, float('inf')):
                     dist[neig_node] = calc
                     heapq.heappush(pq, (calc, neig_node))
-        
-        # Si no encontramos un camino, devolvemos 0.0
+    
         return 0.0
 
                         
