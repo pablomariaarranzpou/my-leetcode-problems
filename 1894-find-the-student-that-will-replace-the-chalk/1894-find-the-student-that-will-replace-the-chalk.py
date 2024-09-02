082b25d96d3b39a1dd618f4f3f3e5b1d35f29e00
Pablo María Arranz Pou <71595730+pablomariaarranzpou@users.noreply.github.com>
2024-09-02 14:21:57 +0200

    Time: 591 ms (20.98%), Space: 30.6 MB (19.92%) - LeetHub

--- 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.py
+++ 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.py
@@ -1,15 +1,25 @@
 class Solution:
     def chalkReplacer(self, chalk: List[int], k: int) -> int:
         
-        sum_chalk = 0
-        for i in range(len(chalk)):
-            sum_chalk += chalk[i]
-            if sum_chalk > k:
+        if len(chalk) == 1:
+            return 0
+        
+       
+        n = len(chalk)
+        sum_chalks = 0
+        
+        for i in range(n): 
+            sum_chalks += chalk[i]
+            
+            if k < sum_chalks:
                 break
-        k = k % sum_chalk
-        for i in range(len(chalk)):
-            if k < chalk[i]:
-                return i
-            k -= chalk[i]
-        return 0
+        
+        k = k % sum_chalks
+        
+        i = 0
+        while chalk[i % n] <= k:
+            k -= chalk[i % n]
+            i += 1
+            
+        return i % n
         
