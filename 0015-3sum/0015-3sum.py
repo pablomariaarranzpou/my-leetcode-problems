521d3b1b08189858c8f543c493abc98f700df1b1
Pablo María Arranz Pou <71595730+pablomariaarranzpou@users.noreply.github.com>
2024-09-06 18:33:05 +0200

    Time: 664 ms (73.89%), Space: 20.7 MB (31.49%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -2,39 +2,46 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         
         nums = sorted(nums)
+        n = len(nums)
         sol = []
         
-        for i in range(len(nums) - 2):
+        
+        for i in range(n - 2):
             
-            if nums[i] > 0:
-                break
-                
-            if i > 0 and nums[i] == nums[i - 1]:
+            if i > 0 and nums[i] == nums[i-1]:
                 continue
             
-            left = i + 1
-            right = len(nums) - 1
+            l = i + 1
+            r = n - 1
             
-            while left < right:
+            
+            while l < r:
                 
-                current_sum = nums[i] + nums[left] + nums[right]
+                calc = nums[i] + nums[l] + nums[r]
                 
-                if current_sum > 0:
-                    right -= 1
+                if calc > 0:
                     
-                elif current_sum < 0:
-                    left += 1
+                    r -= 1
+                    
+                elif calc < 0: 
+                    l += 1
                     
                 else:
-                    sol.append([nums[i], nums[left], nums[right]])
+                    sol.append([nums[i],nums[l], nums[r]])
                     
-                    while left < right and nums[left] == nums[left + 1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right - 1]:
-                        right -= 1
-
-                    left += 1
-                    right -= 1
+                    
+                    l += 1
+                    
+                    while l < r and nums[l] == nums[l-1]:
+                        l += 1
+                        
+                    r -= 1
+                    while l < r and nums[r] == nums[r + 1]:
+                        r -= 1
                     
         return sol
+        
                     
+                
+                
+        
