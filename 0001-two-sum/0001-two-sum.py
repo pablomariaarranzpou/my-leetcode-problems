60d2b13e5554ac6e3a0d6c23efa24912c591f934
Pablo María Arranz Pou <71595730+pablomariaarranzpou@users.noreply.github.com>
2024-09-09 00:31:18 +0200

    Time: 55 ms (80.97%), Space: 17.7 MB (30.47%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -2,26 +2,17 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
         
- 
-        nums = sorted(enumerate(nums), key=lambda x: x[1])
-        i = 0
-        j = len(nums) - 1
+        hashmap = {}
         
-        
-        while i < j:
+        for i in range(len(nums)):
             
-            one = nums[i]
-            two = nums[j]
+            # target = nums[i] + complement
+            complement = target - nums[i]
             
-            suma = one[1] + two[1]
+            if complement in hashmap:
+                return [i, hashmap[complement]]
             
-            if suma == target:
-                return [one[0], two[0]]   
-            elif suma < target:
-                i += 1   
-            else:
-                j -= 1
-                
-                
+            hashmap[nums[i]] = i
+            
         return []
   
