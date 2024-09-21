b7f033ac3ff2a91e480ceae54ea095a72cc7bee7
Pablo María Arranz Pou <71595730+pablomariaarranzpou@users.noreply.github.com>
2024-09-21 13:02:55 +0200

    Time: 82 ms (40.07%), Space: 23.4 MB (53.23%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.py
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.py
@@ -1,13 +1,6 @@
 class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
-
-    
-        nums = [str(i + 1) for i in range(n)]
-
-        nums = sorted(nums)
-
-
-        return [int(i) for i in nums]
+        return [int(i) for i in sorted([str(i + 1) for i in range(n)])]
     
     
         
