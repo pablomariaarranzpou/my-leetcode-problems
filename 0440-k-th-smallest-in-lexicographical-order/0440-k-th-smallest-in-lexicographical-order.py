8a9df379e8ec6f4a468e74710e65c0a11cb1406c
Pablo María Arranz Pou <71595730+pablomariaarranzpou@users.noreply.github.com>
2024-09-22 18:53:52 +0200

    Time: 43 ms (9.95%), Space: 16.6 MB (30.85%) - LeetHub

---
+++ 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def findKthNumber(self, n, k):
+        curr = 1
+        k -= 1
+
+        while k > 0:
+            step = self.steps(n, curr, curr + 1)
+            if step <= k:
+                curr += 1
+                k -= step
+            else:
+                curr *= 10
+                k -= 1
+
+        return curr
+
+    def steps(self, n, prefix1, prefix2):
+        steps = 0
+        while prefix1 <= n:
+            steps += min(n + 1, prefix2) - prefix1
+            prefix1 *= 10
+            prefix2 *= 10
+        return steps
