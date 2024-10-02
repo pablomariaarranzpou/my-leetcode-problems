2843ca0c1d6504622b318356fff4978c1aca7cbd
Pablo María Arranz Pou <71595730+pablomariaarranzpou@users.noreply.github.com>
2024-10-02 21:47:59 +0200

    Time: 46 ms (67.06%), Space: 16.7 MB (63.81%) - LeetHub

---
+++ 1133-largest-unique-number/1133-largest-unique-number.py
@@ -0,0 +1,23 @@
+class Solution:
+    def largestUniqueNumber(self, nums: List[int]) -> int:
+        
+        nums = sorted(nums)[::-1]
+        seen = set()
+        
+        
+        if len(nums) == 1 or nums[1] != nums[0]:
+            return nums[0]
+            
+        
+        
+        for i in range(1, len(nums) - 1):
+            
+            if nums[i] != nums[i - 1] and nums[i + 1] != nums[i]:
+                return nums[i]
+            else:
+                seen.add(nums[i])
+                
+        if nums[-1] != nums[-2]:
+            return nums[-1]
+        
+        return -1
